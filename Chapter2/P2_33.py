b4bdc59e046e2177cf9954ebd503b57ce54d3ec9
Mojca <mojca.bertoncelj@7learnings.com>
2019-10-03 23:39:39 +0200

    combine first derivatives

--- Chapter2/P2_33.py
+++ Chapter2/P2_33.py
@@ -1,18 +1,21 @@
 import re
 
+
+# TODO: doesn't work with negative numbers
 def get_operators(algebraic_notation):
     return re.findall('\+|\-', algebraic_notation)
 
+
 def get_expressions(algebraic_notation):
-    al_no_spaces = algebraic_notation.replace(' ','')
+    al_no_spaces = algebraic_notation.replace(' ', '')
     expressions_list = re.split('\+|\-', al_no_spaces)
     return expressions_list
 
-#TODO: change to work for more then single digits
+
 def first_derivation(expression):
     if 'x' not in expression:
         return '0'
-    elif expression[-1]=='x': #TODO x**1
+    elif expression[-1] == 'x':  # TODO x**1
         return expression.strip('x')
     else:
         coefficient, degree = map(int, expression.split('x**'))
@@ -21,10 +24,19 @@ def first_derivation(expression):
         return new_expression
 
 
+def first_derivation_of_poly(algebraic_notation):
+    operators = get_operators(algebraic_notation)
+    operators.append('')
+    expressions = get_expressions(algebraic_notation)
+    result = 'y='
+    for expresion, operator in zip(expressions, operators):
+        result = result + first_derivation(expresion) + operator
+    return result
+
+
 if __name__ == "__main__":
-    assert(get_operators('2x**3+4x**2-8x+6')==['+', '-', '+'])
-    assert(get_expressions('2x**3+4x**2-8x+6')==['2x**3', '4x**2', '8x', '6'])
-    assert(first_derivation('1')=='0')
-    assert(first_derivation('5x')=='5')
-    print(first_derivation('12x**45'))
-    #first_derivative('9x** 3')
+    assert(get_operators('2x**3+4x**2-8x+6') == ['+', '-', '+'])
+    assert(get_expressions('2x**3+4x**2-8x+6') == ['2x**3', '4x**2', '8x', '6'])
+    assert(first_derivation('6') == '0')
+    assert(first_derivation('5x') == '5')
+    assert(first_derivation_of_poly('2x**3+4x**2-8x+6') == 'y=6x**2+8x**1-8+0')
